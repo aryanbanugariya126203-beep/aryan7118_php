1e6096a53bad3c49f28a8a501d49ac3dfb6378da
aryanbanugariya126203-beep <aryan.banugariya126203@marwadiuniversity.ac.in>
2026-06-09 10:29:10 +0530

    Add files via upload

---
+++ Pro1(92400527118).php
@@ -0,0 +1,17 @@
+<?php
+
+define("COLLEGE", "Marwadi University");
+
+$name = "aryan";
+$semester = "Semester 5";
+$spi = 5.25;
+$cpi = 6.50;
+
+echo "<h2>Previous Semester Result</h2>";
+echo "College Name: " . COLLEGE . "<br>";
+echo "Student Name: " . $name . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "SPI: " . $spi . "<br>";
+echo "CPI: " . $cpi . "<br>";
+
+?>
