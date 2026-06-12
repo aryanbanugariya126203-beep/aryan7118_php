c5f922b4d375fe1a3a6737b3404d35275c303a80
aryanbanugariya126203-beep <aryan.banugariya126203@marwadiuniversity.ac.in>
2026-06-12 09:14:40 +0530

    Add files via upload

---
+++ Pro3(92400527118).php
@@ -0,0 +1,19 @@
+<?php
+
+$a = 20;
+$b = 10;
+
+echo "<h2>Operators in PHP</h2>";
+
+echo "Value of A = " . $a . "<br>";
+echo "Value of B = " . $b . "<br><br>";
+
+echo "<h3>Arithmetic Operators</h3>";
+
+echo "Addition = " . ($a + $b) . "<br>";
+echo "Subtraction = " . ($a - $b) . "<br>";
+echo "Multiplication = " . ($a * $b) . "<br>";
+echo "Division = " . ($a / $b) . "<br>";
+echo "Modulus = " . ($a % $b) . "<br>";
+
+?>
