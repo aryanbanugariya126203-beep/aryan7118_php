0e0fade9f6e139e495c74ac6d32433a359da08b3
aryanbanugariya126203-beep <aryan.banugariya126203@marwadiuniversity.ac.in>
2026-06-17 09:46:20 +0530

    Add files via upload

---
+++ Pro6(9400527118) college information.php
@@ -0,0 +1,12 @@
+<?php
+
+function collegeInfo()
+{
+    echo "College Name: Marwadi umiversity<br>";
+    echo "Course Name: BCA<br>";
+    echo "Academic Year: 2024-2027<br>";
+}
+
+collegeInfo();
+
+?>
