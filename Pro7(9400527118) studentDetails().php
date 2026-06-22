99d440cb2cc7c13b6aabb871157c73a81b351937
aryanbanugariya126203-beep <aryan.banugariya126203@marwadiuniversity.ac.in>
2026-06-22 09:41:38 +0530

    Add files via upload

---
+++ Pro7(9400527118) studentDetails().php
@@ -0,0 +1,12 @@
+<?php
+
+function studentDetails($name, $enrollment, $semester)
+{
+    echo "Student Name: " . $name . "<br>";
+    echo "Enrollment Number: " . $enrollment . "<br>";
+    echo "Semester: " . $semester . "<br>";
+}
+
+studentDetails("banugariya aryan", "92400527118", "Semester 5");
+
+?>
